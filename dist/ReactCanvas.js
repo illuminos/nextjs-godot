4ecd6fad2781da135f94536b1476db8de0e29240
shrag <nateshrager@gmail.com>
2022-07-01 15:55:47 -0700

    initial hacky working version

--- dist/ReactCanvas.js
+++ dist/ReactCanvas.js
@@ -7,7 +7,6 @@ function toFailure(err) {
     return { msg: msg, mode: "notice", initialized: true };
 }
 var ReactCanvas = function (_a) {
-    //console.log(_a)  //should be .pck object
     var engine = _a.engine, pck = _a.pck, _b = _a.width, width = _b === void 0 ? 480 : _b, _c = _a.height, height = _c === void 0 ? 300 : _c;
     var canvasRef = useRef(null);
     var _d = useState(), instance = _d[0], setInstance = _d[1];
@@ -23,28 +22,26 @@ var ReactCanvas = function (_a) {
     }, [engine]);
     useEffect(function () {
         if (instance) {
-           // console.log(instance)  //should be Engine object
             instance
                 .startGame(pck)
                 .then(function () {
                 changeLoadingState({ mode: "hidden", initialized: true });
             })
-              .catch(function (err) { return changeLoadingState(toFailure(err)); });
-           /* instance.setProgressFunc(function (current, total) {
-                if (total > 0) {
-                    changeLoadingState({ mode: "progress", percent: current / total });
-                }
-                else {
-                    changeLoadingState({ mode: "indeterminate" });
-                }
-            }); */
+                .catch(function (err) { return changeLoadingState(toFailure(err)); });
+            /*     instance.setProgressFunc((current, total) => {
+                  if (total > 0) {
+                    changeLoadingState({ mode: "progress", percent: current / total })
+                  } else {
+                    changeLoadingState({ mode: "indeterminate" })
+                  }
+                }) */
         }
     }, [instance, pck, changeLoadingState]);
-    /* useEffect(function () {
-        if (instance) {
-            instance.setCanvas(canvasRef.current);
-        }
-    }, [instance, canvasRef.current]); */
+    /* useEffect(() => {
+      if (instance) {
+        instance.setCanvas(canvasRef.current)
+      }
+    }, [instance, canvasRef.current]) */
     return (React.createElement("canvas", { ref: canvasRef, id: "canvas", width: width, height: height, style: { display: loadingState.initializing ? "hidden" : "block" } },
         "HTML5 canvas appears to be unsupported in the current browser.",
         React.createElement("br", null),
--- dist/index.js
+++ dist/index.js
@@ -6,26 +6,19 @@ import * as React from "react";
 import { useEffect, useRef, useState } from "react";
 import AsyncLoading from "./AsyncLoading";
 import ReactCanvas from "./ReactCanvas";
-//if (typeof window !== "undefined") {  }
-// https://github.com/vercel/next.js/discussions/32236#discussioncomment-1766808   //Try this here
-/*  
- console.log(fs.readFile(path.join(serverRuntimeConfig.PROJECT_ROOT, './public/static/tutd.js')).toString())
-
- */
-    var useScript = function (url, onLoad) {
-        useEffect(function () {
-           var script = document.createElement("script");
-            script.src = url;
-            script.async = true;
-            script.onload = onLoad;
-            document.body.appendChild(script);
-            return function () {
-                document.body.removeChild(script);
-            };
-        }, [url]);
-    };
-
-var NextGodot = function (props) {
+var useScript = function (url, onLoad) {
+    useEffect(function () {
+        var script = document.createElement("script");
+        script.src = url;
+        script.async = true;
+        script.onload = onLoad;
+        document.body.appendChild(script);
+        return function () {
+            document.body.removeChild(script);
+        };
+    }, [url]);
+};
+var ReactGodot = function (props) {
     var script = props.script, pck = props.pck, _a = props.resize, resize = _a === void 0 ? false : _a, width = props.width, height = props.height, params = props.params;
     var outerRef = useRef(null);
     var _b = useState(null), engine = _b[0], setEngine = _b[1];
@@ -42,8 +35,8 @@ var NextGodot = function (props) {
             ]);
         }
     }, [resize, outerRef.current]);
-    return (React.createElement("div", { id: "head", ref: outerRef },
+    return (React.createElement("div", { id: "wrap", ref: outerRef },
         React.createElement(AsyncLoading, null, engine && (React.createElement(ReactCanvas, { pck: pck, engine: engine, width: dimensions[0], height: dimensions[1], params: params })))));
 };
-export default NextGodot;
+export default ReactGodot;
 //# sourceMappingURL=index.js.map
